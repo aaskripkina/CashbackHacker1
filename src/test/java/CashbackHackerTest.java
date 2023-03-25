0e6bc5e992fe009b62fa4cb30d944bb22f0b249f
Alexandra Skripkina <aa.skripkina@gmail.com>
2023-03-25 18:31:03 +0200

    added CashbackHackerTest

---
+++ src/test/java/CashbackHackerTest.java
@@ -0,0 +1,30 @@
+import ru.netology.service.CashbackHacker;
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+
+
+public class CashbackHackerTest {
+    CashbackHacker service = new CashbackHacker();
+    @Test
+    public void AmountLessThan1000() {
+        int actual = service.remain(500);
+        int expected = 500;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void AmountMoreThan1000() {
+        int actual = service.remain(1100);
+        int expected = 900;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void AmountEqualTo1000() {
+        int actual = service.remain(1000);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+}
